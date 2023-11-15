a13a5a1809b7118560f549b4381a11d353175086
Samuel Gillespie <samuel.gillespie@sydney.edu.au>
2023-11-15 21:19:20 +1100

    POC web server with audio player

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::fs::File;
-use std::io::BufReader;
 use rodio::{Decoder, OutputStream, Sink, Source};
+
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // Get a output stream handle to the default physical sound device
@@ -8,7 +12,7 @@ fn main() {
     let sink = Sink::try_new(&stream_handle).unwrap();
 
     // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("samples/noise.mp3").unwrap());
+    let file = BufReader::new(fs::File::open("samples/noise.mp3").unwrap());
     // Decode that sound file into a source
     let source = Decoder::new(file).unwrap().buffered().repeat_infinite();
     sink.append(source);
@@ -16,5 +20,43 @@ fn main() {
     // // Play the sound directly on the device
     // stream_handle.play_raw(source.convert_samples());
 
-    sink.sleep_until_end();
+    // Let the webserver do the
+    // sink.sleep_until_end();
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // println!("Connection established!");
+        if sink.is_paused() {
+            sink.play()
+        }
+        else {
+            sink.pause();
+        }
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    let (status, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    }
+    else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    println!("[{request_line}] => [{status}] {file_name}");
+
+    let content = fs::read_to_string(file_name).unwrap();
+    let length = content.len();
+
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
